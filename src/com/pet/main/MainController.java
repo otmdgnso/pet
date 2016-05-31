f356e7dbf90da5479a5f68eae836be6535ec8dc6
jihunKing <jihun5008@naver.com>
2016-05-31 14:04:53 +0900

    232332

--- src/com/pet/main/MainController.java
+++ src/com/pet/main/MainController.java
@@ -1,15 +1,10 @@
 package com.pet.main;
 
-import java.net.URLEncoder;
-import java.util.Currency;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +12,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.pet.adopt.Adopt;
 import com.pet.adopt.AdoptService;
+import com.pet.house.House;
+import com.pet.house.HouseService;
 import com.pet.photo.Photo;
 import com.pet.photo.PhotoService;
 
@@ -26,6 +23,8 @@ public class MainController {
 	private PhotoService photoService;
 	@Autowired
 	private AdoptService adoptService;
+	@Autowired
+	private HouseService houseService;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView mainPage() throws Exception{		
@@ -38,10 +37,12 @@ public class MainController {
 		
 		List<Photo> list=photoService.mainListPhoto(map);
 		List<Adopt> listAdopt=adoptService.listPreSale(map);
+		List<House> listHouse=houseService.listHouse(map);
 				
 		ModelAndView mav=new ModelAndView(".mainLayout");
 		mav.addObject("list",list);
 		mav.addObject("listAdopt",listAdopt);
+		mav.addObject("listHouse",listHouse);
 		return mav;
 	}	
 	
