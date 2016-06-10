24a8e15e83a694f04cfe24f795996ca68d25f206
hh <01234590@naver.com>
2016-06-10 10:41:23 +0900

    qna

--- src/com/pet/qna/QnaController.java
+++ src/com/pet/qna/QnaController.java
@@ -1,16 +1,151 @@
 package com.pet.qna;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.pet.common.MyUtil;
 
 @Controller("qna.qnaController")
 public class QnaController {
+	@Autowired
+	private QnaService service;
+	@Autowired
+	private MyUtil myUtil;
 
-	@RequestMapping(value="/qna/qna")
-	public ModelAndView list() throws Exception {
+	@RequestMapping(value="/qna/list")
+	public ModelAndView list(
+			HttpServletRequest req,
+			@RequestParam(value="page", defaultValue="1") int current_page,
+			@RequestParam(value="searchKey", defaultValue="subject") String searchKey,
+			@RequestParam(value="searchValue", defaultValue="") String searchValue
+			) throws Exception {
 		
-		ModelAndView mav=new ModelAndView(".qna.qna");
+		String cp = req.getContextPath();
+		
+		int numPerPage=10;
+		int total_page=0;
+		int dataCount=0;
+		
+		if(req.getMethod().equalsIgnoreCase("GET")) {
+			searchValue=URLDecoder.decode(searchValue, "utf-8");
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("searchKey", searchKey);
+		map.put("searchValue", searchValue);
+		
+		dataCount=service.dataCount(map);
+		if(dataCount!=0)
+			total_page=myUtil.pageCount(numPerPage, dataCount);
+		
+		// 다른 사람이 자료를 삭제하여 전체 페이지수가 변화 된 경우
+        if(total_page < current_page) 
+            current_page = total_page;
+
+        // 리스트에 출력할 데이터를 가져오기
+        int start = (current_page - 1) * numPerPage + 1;
+        int end = current_page * numPerPage;
+        map.put("start", start);
+        map.put("end", end);
+        
+        //글 리스트
+        List<Qna> list=service.listQna(map);
+        
+        //리스트 번호
+        int listNum, n=0;
+        Iterator<Qna> it=list.iterator();
+        while(it.hasNext()) {
+            Qna data = it.next();
+            listNum = dataCount - (start + n - 1);
+            data.setListNum(listNum);
+            n++;
+        }
+        
+        String params = "";
+        String urlList = cp+"/qna/list";
+        String urlArticle = cp+"/qna/article?page=" + current_page;
+        if(searchValue.length()!=0) {
+        	params = "searchKey=" +searchKey + 
+        	             "&searchValue=" + URLEncoder.encode(searchValue, "utf-8");	
+        }
+        
+        if(params.length()!=0) {
+            urlList = cp+"/qna/list?" + params;
+            urlArticle = cp+"/qna/article?page=" + current_page + "&"+ params;
+        }
+
+        ModelAndView mav=new ModelAndView(".qna.list");
+        mav.addObject("list", list);
+        mav.addObject("urlArticle", urlArticle);
+        mav.addObject("page", current_page);
+        mav.addObject("dataCount", dataCount);
+        mav.addObject("total_page", total_page);
+        mav.addObject("paging", myUtil.paging(current_page, total_page, urlList));		
+		
+		return mav;
+	}
+	
+	@RequestMapping(value="/qna/created", method=RequestMethod.GET)
+	public ModelAndView createdForm(
+			HttpSession session
+			) throws Exception {
+		
+		ModelAndView mav=new ModelAndView("qna.created");
+		mav.addObject("mode", "created");
+		return mav;
+	}
+	
+	@RequestMapping(value="/qna/created")
+	public ModelAndView createdSubmit(
+			HttpSession session,
+			Qna dto
+			) throws Exception {
+		
+		service.insertQna(dto);
+		
+		return new ModelAndView("redirect:/qna/list");
+	}
+	
+	@RequestMapping(value="/qna/article")
+	public ModelAndView article(
+			@RequestParam(value="qNum") int qNum,
+			@RequestParam(value="page") String page,
+			@RequestParam(value="searchKey", defaultValue="subject") String searchKey,
+			@RequestParam(value="searchValue", defaultValue="") String searchValue
+			) throws Exception {
+		
+		searchValue=URLDecoder.decode(searchValue, "utf-8");
+		
+		//조회수 증가
+		service.updateHitCount(qNum);
+		
+		//해당 레코드 가져오기
+		Qna dto=service.readQna(qNum);
+		if(dto==null)
+			return new ModelAndView("redirect:/qna/list?page="+page);
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("searchKey", searchKey);
+		map.put("searchValue", searchValue);
+		map.put("qNum", qNum);
+		
+		ModelAndView mav=new ModelAndView(".qna.article");
+		mav.addObject("dto", dto);
+		mav.addObject("page", page);
 		return mav;
 	}
 }
---
+++ src/com/pet/qna/QnaServiceImpl.java
@@ -0,0 +1,94 @@
+package com.pet.qna;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pet.common.dao.CommonDAO;
+
+@Service("qna.qnaService")
+public class QnaServiceImpl implements QnaService{
+	@Autowired
+	private CommonDAO dao;
+	
+	@Override
+	public int insertQna(Qna dto) {
+		int result=0;
+		try {
+			result=dao.insertData("qna.insertQna", dto);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
+	}
+
+	@Override
+	public List<Qna> listQna(Map<String, Object> map) {
+		List<Qna> list=null;
+		try {
+			list=dao.getListData("qna.listQna", map);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return list;
+	}
+
+	@Override
+	public int dataCount(Map<String, Object> map) {
+		int result=0;
+		try {
+			result=dao.getIntValue("qna.dataCount", map);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
+	}
+
+	@Override
+	public Qna readQna(int qNum) {
+		Qna dto=null;
+		try {
+			dto=dao.getReadData("qna.readQna", qNum);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return dto;
+	}
+
+	@Override
+	public int updateHitCount(int qNum) {
+		int result=0;
+		try {
+			result=dao.updateData("qna.updateHitCount", qNum);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
+	}
+
+	@Override
+	public int updateQna(Qna dto) {
+		int result=0;
+		try {
+			dao.updateData("qna.updateQna", dto);
+			result=1;
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
+	}
+
+	@Override
+	public int deleteQna(int qNum) {
+		int result=0;
+		try {
+			dao.deleteData("qna.deleteQna", qNum);
+			result=1;
+		} catch (Exception e) {
+		}
+		return result;
+	}
+
+}
