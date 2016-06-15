002b13826269e81a1a1209234d4139e31df844d6
hh <01234590@naver.com>
2016-06-15 16:25:58 +0900

    수정

--- src/com/pet/member/MemberService.java
+++ src/com/pet/member/MemberService.java
@@ -1,18 +1,10 @@
 package com.pet.member;
 
-import java.util.List;
-import java.util.Map;
-
 public interface MemberService {
 	 public int insertMemeber(Member dto, String pathname);	
 	 public Member readMember(String userId);
 	 public int updateMember(Member dto,String pathname);
 	 public int updateTheme(Member dto, String pathname);
 	 public int deleteMember(String userId);
-	 
-	 //ºÏ¸¶Å©
-	 public int insertBookmart(Bookmark dto);
-	 public List<Bookmark> listBookmark(Map<String, Object> map);
-	 public int dataCount(Map<String, Object> map);
-	 public int deleteBookmark(int bookmarkNum);
+
 }
